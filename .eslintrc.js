bb2f2d6b118a3e691be0500d6e6aa6b7e8faa91e
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2020-02-23 09:21:20 +0530

    Added npm ignore and liscense

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,28 @@
 module.exports = {
   env: {
-    commonjs: true,
+    browser: true,
     es6: true,
+    jest: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: 'airbnb',
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: [
+    'react',
+    'babel'
+  ],
   rules: {
+    'react/jsx-filename-extension': 0,
+    // Babel AST transform is not pure
+    'no-param-reassign': 0
   },
 };

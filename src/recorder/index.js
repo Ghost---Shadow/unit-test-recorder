c619c125109b8e6836bb6a45e39a1deba0420631
Souradeep Nanda <souradeep.nanda123@gmail.com>
2020-03-14 12:59:55 +0530

    Refactor out dependency injection

--- src/recorder/index.js
+++ src/recorder/index.js
@@ -1,50 +1,12 @@
-const { flatten, unflatten } = require('flat');
 const _ = require('lodash');
 
 const RecorderManager = require('./manager');
 const { mockRecorderWrapper } = require('./mock');
+const { injectDependencyInjections } = require('./injection');
 
 const sanitize = (obj) => {
   if (typeof (obj) === 'function') return obj.toString();
   return obj;
-};
-
-const recordInjectedActivity = (idObj, paramIds, index, fppkey, paramsOfInjected, result) => {
-  const { path, name, captureIndex } = idObj;
-  const fqn = `${paramIds[index]}.${fppkey}`;
-  const destinationPath = ['recorderState', path, 'exportedFunctions', name, 'captures', captureIndex, 'injections', fqn];
-  if (!_.get(RecorderManager, destinationPath)) {
-    _.set(RecorderManager, destinationPath, []);
-  }
-  RecorderManager.recorderState[path].exportedFunctions[name]
-    .captures[captureIndex].injections[fqn].push({ params: paramsOfInjected, result });
-};
-
-const injectDependencyInjections = (params, paramIds, idObj) => {
-  params.forEach((param, index) => {
-    if (typeof (param) === 'object') {
-      const flatObj = flatten(param);
-      Object.keys(flatObj).forEach((fppkey) => {
-        if (typeof (flatObj[fppkey]) === 'function') {
-          const oldFp = flatObj[fppkey];
-          flatObj[fppkey] = (...paramsOfInjected) => {
-            const result = oldFp(...paramsOfInjected);
-            if (typeof (result.then) === 'function') {
-              // It might be a promise
-              result.then((res) => {
-                recordInjectedActivity(idObj, paramIds, index, fppkey, paramsOfInjected, res);
-              });
-            } else {
-              recordInjectedActivity(idObj, paramIds, index, fppkey, paramsOfInjected, result);
-            }
-            return result;
-          };
-        }
-      });
-      const newParam = unflatten(flatObj);
-      params[index] = newParam;
-    }
-  });
 };
 
 const pre = ({ meta, p }) => {

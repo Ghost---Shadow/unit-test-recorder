6b88d30621e2fb8474afac03a2a8449d32d818dc
Souradeep Nanda <souradeep.nanda123@gmail.com>
2020-03-25 17:33:36 +0530

    Dont drop non-cycles

--- src/recorder/utils.js
+++ src/recorder/utils.js
@@ -1,17 +1,6 @@
-const safeStringify = (obj) => {
-  // https://stackoverflow.com/a/11616993/1217998
-  const cache = [];
-  return JSON.stringify(obj, (key, value) => {
-    if (typeof value === 'object' && value !== null) {
-      if (cache.indexOf(value) !== -1) {
-        // Duplicate reference found, discard key
-        return undefined;
-      }
-      // Store value in our collection
-      cache.push(value);
-    }
-    return value;
-  }, 2);
-};
+const stringify = require('json-stringify-safe');
+
+// https://stackoverflow.com/a/30204271/1217998
+const safeStringify = obj => stringify(obj, null, 2, () => undefined);
 
 module.exports = { safeStringify };

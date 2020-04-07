35d05389cf4544b6fe191822839ff0fdd0d08717
Souradeep Nanda <souradeep.nanda123@gmail.com>
2020-04-07 18:40:51 +0530

    Comprehensive blacklist

--- src/plugin/blacklist-generator.js
+++ src/plugin/blacklist-generator.js
@@ -1,11 +1,42 @@
 // https://stackoverflow.com/a/42658586/1217998
 const getBlackListForProperty = type => Object
-  .getOwnPropertyNames(type.prototype)
+  .getOwnPropertyNames(type.prototype || Object)
   .filter(prop => !{ caller: true, callee: true, arguments: true }[prop])
-  .filter(prop => typeof type.prototype[prop] === 'function');
+  .filter((prop) => {
+    try {
+      return typeof type.prototype[prop] === 'function';
+    } catch (e) {
+      return true;
+    }
+  });
 
 const getBlackList = () => {
-  const types = [Array, String, Object, Number, Date, Function];
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+  const types = [
+    // Primitives
+    Boolean, Number, String, /* BigInt, */ Symbol,
+    // Object likes
+    Object, Array, Map, Set, WeakMap, WeakSet, Date,
+    // Arrays
+    Int8Array, Uint8Array, Uint8ClampedArray, Int16Array,
+    Uint16Array, Int32Array, Uint32Array, Float32Array,
+    Float64Array, /* BigInt64Array, BigUint64Array, */
+    // Function
+    Function,
+    // Global properties
+    Error, RegExp, Math,
+    // Global Structured data
+    ArrayBuffer, SharedArrayBuffer, Atomics, DataView, JSON,
+    // Control abstraction
+    Promise, /* Generator, GeneratorFunction, AsyncFunction, Iterator, AsyncIterator, */
+    // Reflection
+    Reflect, Proxy,
+    // Intl
+    Intl,
+    // WebAssembly
+    /* WebAssembly, */
+  ];
   const allProperties = types.reduce((acc, type) => {
     const keys = getBlackListForProperty(type);
     const obj = keys.reduce((innerAcc, key) => ({ ...innerAcc, [key]: true }), {});

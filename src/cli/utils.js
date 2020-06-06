383bc603b06a50b8e5d77a20e00d50fe9bb0d80e
Souradeep Nanda <souradeep.nanda123@gmail.com>
2020-06-06 18:20:42 +0530

    Parse tsconfig fix

--- src/cli/utils.js
+++ src/cli/utils.js
@@ -1,12 +1,17 @@
 const cp = require('child_process');
 const fs = require('fs');
+const ts = require('typescript');
 
 const rimraf = require('rimraf');
 
 const compileAndGetOutputDir = (typescriptConfig) => {
   if (fs.existsSync(typescriptConfig)) {
     console.log(`Typescript config found at ${typescriptConfig}`);
-    const config = JSON.parse(fs.readFileSync(typescriptConfig).toString());
+    const contents = fs.readFileSync(typescriptConfig).toString();
+    const { config, error } = ts.parseConfigFileTextToJson(typescriptConfig, contents);
+
+    if (error) throw new Error(error);
+
     const tsBuildDir = config.compilerOptions.outDir;
     console.log('Purging build directory');
     rimraf.sync(tsBuildDir);

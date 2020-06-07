c3d29003af1291fbc189d34fc9eb22ae4175ba1b
Souradeep Nanda <souradeep.nanda123@gmail.com>
2020-06-07 19:03:47 +0530

    MockImportBlock typescript support

--- src/generator/components/MockImportBlock/MockImportBlock.js
+++ src/generator/components/MockImportBlock/MockImportBlock.js
@@ -4,21 +4,52 @@ const { DefaultImportStatement } = require('../ImportStatements/ImportStatements
 
 const JestMockStatement = ({ importPath }) => `jest.mock('${importPath}');`;
 
+const SpecialImportStatement = ({ importPath, packagedArguments }) => {
+  const identifier = _.camelCase(importPath);
+  const { isTypescript } = packagedArguments;
+  if (!isTypescript) {
+    return DefaultImportStatement({ importPath, identifier, packagedArguments });
+  }
+  const newIdentifier = `${identifier}Original`;
+  const importStatement = DefaultImportStatement({
+    importPath,
+    identifier: newIdentifier,
+    packagedArguments,
+  });
+  return importStatement;
+};
+
+const ReassignmentStatement = ({ importPath, packagedArguments }) => {
+  const { isTypescript } = packagedArguments;
+  if (!isTypescript) return '';
+
+  const identifier = _.camelCase(importPath);
+  const newIdentifier = `${identifier}Original`;
+
+  return `const ${identifier} = ${newIdentifier} as any`;
+};
+
 const MockImportBlock = ({ meta, packagedArguments }) => {
   const mockStatements = meta.mocks
     .map(importPath => JestMockStatement({ importPath }));
 
   const importStatements = meta.mocks
-    .map(importPath => DefaultImportStatement({
+    .map(importPath => SpecialImportStatement({
       importPath,
-      identifier: _.camelCase(importPath),
+      packagedArguments,
+    }));
+
+  const reassignmentStatements = meta.mocks
+    .map(importPath => ReassignmentStatement({
+      importPath,
       packagedArguments,
     }));
 
   const importStatementStr = importStatements.join('\n');
+  const reassignmentStatementStr = reassignmentStatements.join('\n');
   const mockStatementsStr = mockStatements.join('\n');
 
-  return `${importStatementStr}\n\n${mockStatementsStr}`;
+  return `${importStatementStr}\n\n${reassignmentStatementStr}\n\n${mockStatementsStr}`;
 };
 
 module.exports = {
--- src/generator/components/MockImportBlock/MockImportBlock.test.js
+++ src/generator/components/MockImportBlock/MockImportBlock.test.js
@@ -11,32 +11,69 @@ describe('MockImportBlock', () => {
     });
   });
   describe('MockImportBlock', () => {
-    it('should generate code', () => {
-      const props = {
-        meta: {
-          mocks: ['m1', 'm2', 'm3', 'm4', 'm5'],
-        },
-        packagedArguments: {},
-      };
-      const code = MockImportBlock(props);
-      const formattedCode = prettier.format(code, {
-        singleQuote: true,
-        parser: 'babel',
-      });
-      expect(formattedCode).toMatchInlineSnapshot(`
-        "const m1 = require('m1');
-        const m2 = require('m2');
-        const m3 = require('m3');
-        const m4 = require('m4');
-        const m5 = require('m5');
+    describe('javascript', () => {
+      it('should generate code', () => {
+        const props = {
+          meta: {
+            mocks: ['m1', 'm2', 'm3', 'm4', 'm5'],
+          },
+          packagedArguments: {},
+        };
+        const code = MockImportBlock(props);
+        const formattedCode = prettier.format(code, {
+          singleQuote: true,
+          parser: 'babel',
+        });
+        expect(formattedCode).toMatchInlineSnapshot(`
+          "const m1 = require('m1');
+          const m2 = require('m2');
+          const m3 = require('m3');
+          const m4 = require('m4');
+          const m5 = require('m5');
 
-        jest.mock('m1');
-        jest.mock('m2');
-        jest.mock('m3');
-        jest.mock('m4');
-        jest.mock('m5');
-        "
-      `);
+          jest.mock('m1');
+          jest.mock('m2');
+          jest.mock('m3');
+          jest.mock('m4');
+          jest.mock('m5');
+          "
+        `);
+      });
+    });
+    describe('typescript', () => {
+      it('should generate code', () => {
+        const props = {
+          meta: {
+            mocks: ['m1', 'm2', 'm3', 'm4', 'm5'],
+          },
+          packagedArguments: { isTypescript: true },
+        };
+        const code = MockImportBlock(props);
+        const formattedCode = prettier.format(code, {
+          singleQuote: true,
+          parser: 'typescript',
+        });
+        expect(formattedCode).toMatchInlineSnapshot(`
+          "import * as m1Original from 'm1';
+          import * as m2Original from 'm2';
+          import * as m3Original from 'm3';
+          import * as m4Original from 'm4';
+          import * as m5Original from 'm5';
+
+          const m1 = m1Original as any;
+          const m2 = m2Original as any;
+          const m3 = m3Original as any;
+          const m4 = m4Original as any;
+          const m5 = m5Original as any;
+
+          jest.mock('m1');
+          jest.mock('m2');
+          jest.mock('m3');
+          jest.mock('m4');
+          jest.mock('m5');
+          "
+        `);
+      });
     });
   });
 });
